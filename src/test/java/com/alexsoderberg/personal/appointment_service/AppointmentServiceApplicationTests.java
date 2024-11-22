42364e0a56b28c2b92ad75ea95ce1ba358837e59
Alex Söderberg <as228gc@student.lnu.se>
2024-11-22 11:55:40 +0100

    Initialize Spring Boot project

---
+++ src/test/java/com/alexsoderberg/personal/appointment_service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexsoderberg.personal.appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
