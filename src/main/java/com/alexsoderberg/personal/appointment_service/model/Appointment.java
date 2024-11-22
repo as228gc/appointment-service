1771d3665ec21a5c33931c233884ed6ef9e9bdae
Alex Söderberg <as228gc@student.lnu.se>
2024-11-22 14:42:05 +0100

    Feat: Add method stubs for name setter and getter

---
+++ src/main/java/com/alexsoderberg/personal/appointment_service/model/Appointment.java
@@ -0,0 +1,30 @@
+package com.alexsoderberg.personal.appointment_service.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Appointment {
+  private String name;
+  private LocalDate date;
+  private LocalTime time;
+  private int duration;
+  private String description;
+
+  public Appointment(String name, LocalDate date, LocalTime time, int duration, String description) {
+    this.name = name;
+    this.date = date;
+    this.time = time;
+    this.duration = duration;
+    this.description = description;
+  }
+
+  public String getName() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getName'");
+  }
+
+  public void setName(String newName) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setName'");
+  }
+}
