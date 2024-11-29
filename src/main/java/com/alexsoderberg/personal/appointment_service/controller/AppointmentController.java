587fe9a3854af042471695e556c48c4fd40a870a
Alex Söderberg <as228gc@student.lnu.se>
2024-11-29 12:13:21 +0100

    GREEN: Implement the createAppointment method

--- src/main/java/com/alexsoderberg/personal/appointment_service/controller/AppointmentController.java
+++ src/main/java/com/alexsoderberg/personal/appointment_service/controller/AppointmentController.java
@@ -1,5 +1,27 @@
 package com.alexsoderberg.personal.appointment_service.controller;
 
-public class AppointmentController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.alexsoderberg.personal.appointment_service.model.Appointment;
+import com.alexsoderberg.personal.appointment_service.service.AppointmentService;
+
+@RestController
+@RequestMapping("/api/appointments")
+public class AppointmentController {
+  private AppointmentService service;
+
+  public AppointmentController(AppointmentService service) {
+    this.service = service;
+  }
+
+  @PostMapping
+  public ResponseEntity<Appointment> createAppointment(@RequestBody Appointment appointment) {
+    Appointment savedAppointment = service.saveAppointment(appointment);
+    return new ResponseEntity<>(savedAppointment, HttpStatus.CREATED);
+  }
 }
