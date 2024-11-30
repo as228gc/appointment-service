42b284add180abac81820bf45a064b94c6f6f5a4
Alex Söderberg <as228gc@student.lnu.se>
2024-11-30 13:05:05 +0100

    GREEN: Implement delete appointment in controller

--- src/main/java/com/alexsoderberg/personal/appointment_service/controller/AppointmentController.java
+++ src/main/java/com/alexsoderberg/personal/appointment_service/controller/AppointmentController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +34,11 @@ public class AppointmentController {
   public ResponseEntity<List<Appointment>> getAppointments() {
     List<Appointment> appointments = service.getAppointments();
     return new ResponseEntity<>(appointments, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Appointment> deleteAppointment(@PathVariable Long id) {
+    Appointment deletedAppointment = service.deleteAppointment(id);
+    return new ResponseEntity<>(deletedAppointment, HttpStatus.OK);
   }
 }
